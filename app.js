8d377b2b3faded6c63bd4556ef5b2b4bc8fefaa4
jrmcgarvey <jmcgarve@bellsouth.net>
2022-09-28 13:50:28 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+express = require('express')
+
+const app = express()
+
+app.use(express.static('public'))
+
+const port=5000
+
+app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
